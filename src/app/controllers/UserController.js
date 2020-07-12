ca54665e3b0db992f1d7978de28768c2c1c55945
Gustavo Duarte <gduartemenezes@gmail.com>
2020-07-12 16:53:15 -0300

    Update UserCtrl-Add email verification

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -3,22 +3,37 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    // create yup schema
     const schema = Yup.object().shape({
       name: Yup.string().required(),
       email: Yup.string().required(),
       password: Yup.string().required().min(6),
     });
+    // Validate req.body
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Invalid data, try again' });
     }
     try {
-      const { name, email } = await User.create(req.body);
+      const { email } = req.body;
+      // Check if email is alreay in use
+      const userExists = await User.findOne({
+        where: email,
+      });
+      if (userExists) {
+        return res.status(400).json({
+          message: 'E-mail alredy in use',
+        });
+      }
+      // Create user and return a json with the new user info
+      const { id, name } = await User.create(req.body);
 
       return res.json({
+        id,
         name,
         email,
       });
     } catch (err) {
+      // Return any request errors produced
       return res.status(400).json({
         message: 'Request failed',
         error: err,
