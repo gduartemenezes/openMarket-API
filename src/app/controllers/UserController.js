2687716ca56a1ea30e331eb2c38a5e3fa2286497
Gustavo Duarte <gduartemenezes@gmail.com>
2020-07-13 10:01:30 -0300

    Add UserCtrl.update req and route

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -40,6 +40,41 @@ class UserController {
       });
     }
   }
+
+  async update(req, res) {
+    // TO-DO, after auth middleware
+    try {
+      const { email, oldPassword } = req.body;
+
+      const user = await User.findByPk(req.userId);
+
+      if (email !== user.email) {
+        const userExists = User.findOne({ where: { email } });
+
+        if (userExists)
+          return res.status(401).json({ error: 'E-mail already in use' });
+      }
+
+      if (oldPassword && !(await user.checkPassword(oldPassword))) {
+        return res
+          .status(401)
+          .json({ error: 'Request Failed, currently password does not match' });
+      }
+
+      const { id, name } = await user.update(req.body);
+
+      return res.json({
+        id,
+        name,
+        email,
+      });
+    } catch (err) {
+      return res.status(400).json({
+        message: 'Request Failed',
+        error: err,
+      });
+    }
+  }
 }
 
 export default new UserController();
